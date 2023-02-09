fdd59e103fb533f690b9f19097b8ff892cf80fb6
Vexelior <67873187+Vexelior@users.noreply.github.com>
2023-02-08 21:17:03 -0800

    init, good start
    Error: The SSL connection could not be established.

--- App/Repo Downloader/Form1.Designer.cs
+++ App/Repo Downloader/Form1.Designer.cs
@@ -31,29 +31,93 @@ namespace Repo_Downloader
         {
             this.submitButton = new System.Windows.Forms.Button();
             this.urlEntry = new System.Windows.Forms.TextBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.outputBox = new System.Windows.Forms.TextBox();
+            this.title = new System.Windows.Forms.Label();
+            this.savePathEntry = new System.Windows.Forms.TextBox();
+            this.pathButton = new System.Windows.Forms.Button();
+            this.progressLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // submitButton
             // 
-            this.submitButton.Location = new System.Drawing.Point(327, 232);
+            this.submitButton.Location = new System.Drawing.Point(325, 139);
             this.submitButton.Name = "submitButton";
             this.submitButton.Size = new System.Drawing.Size(156, 43);
             this.submitButton.TabIndex = 0;
             this.submitButton.Text = "Submit";
             this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.Download);
             // 
             // urlEntry
             // 
-            this.urlEntry.Location = new System.Drawing.Point(197, 161);
+            this.urlEntry.Location = new System.Drawing.Point(63, 73);
             this.urlEntry.Name = "urlEntry";
-            this.urlEntry.Size = new System.Drawing.Size(430, 23);
+            this.urlEntry.Size = new System.Drawing.Size(693, 23);
             this.urlEntry.TabIndex = 1;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 424);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(776, 14);
+            this.progressBar.TabIndex = 2;
+            // 
+            // outputBox
+            // 
+            this.outputBox.Location = new System.Drawing.Point(12, 188);
+            this.outputBox.Multiline = true;
+            this.outputBox.Name = "outputBox";
+            this.outputBox.ReadOnly = true;
+            this.outputBox.Size = new System.Drawing.Size(776, 208);
+            this.outputBox.TabIndex = 3;
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Adobe Fan Heiti Std B", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.title.Location = new System.Drawing.Point(282, 25);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(268, 30);
+            this.title.TabIndex = 4;
+            this.title.Text = "Repository Downloader";
+            // 
+            // savePathEntry
+            // 
+            this.savePathEntry.Location = new System.Drawing.Point(63, 110);
+            this.savePathEntry.Name = "savePathEntry";
+            this.savePathEntry.Size = new System.Drawing.Size(693, 23);
+            this.savePathEntry.TabIndex = 5;
+            // 
+            // pathButton
+            // 
+            this.pathButton.Location = new System.Drawing.Point(763, 110);
+            this.pathButton.Name = "pathButton";
+            this.pathButton.Size = new System.Drawing.Size(25, 23);
+            this.pathButton.TabIndex = 6;
+            this.pathButton.Text = "...";
+            this.pathButton.UseVisualStyleBackColor = true;
+            this.pathButton.Click += new System.EventHandler(this.PopulateDownloadPath);
+            // 
+            // progressLabel
+            // 
+            this.progressLabel.AutoSize = true;
+            this.progressLabel.Location = new System.Drawing.Point(390, 406);
+            this.progressLabel.Name = "progressLabel";
+            this.progressLabel.Size = new System.Drawing.Size(0, 15);
+            this.progressLabel.TabIndex = 7;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.progressLabel);
+            this.Controls.Add(this.pathButton);
+            this.Controls.Add(this.savePathEntry);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.progressBar);
             this.Controls.Add(this.urlEntry);
             this.Controls.Add(this.submitButton);
             this.Name = "Form1";
@@ -67,6 +131,12 @@ namespace Repo_Downloader
 
         private System.Windows.Forms.Button submitButton;
         private System.Windows.Forms.TextBox urlEntry;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.TextBox outputBox;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.TextBox savePathEntry;
+        private System.Windows.Forms.Button pathButton;
+        private System.Windows.Forms.Label progressLabel;
     }
 }
 
--- App/Repo Downloader/Form1.cs
+++ App/Repo Downloader/Form1.cs
@@ -7,6 +7,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.Net.Http;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
 
 namespace Repo_Downloader
 {
@@ -17,5 +22,74 @@ namespace Repo_Downloader
             InitializeComponent();
         }
 
+        public static void BypassCertificateValidation()
+        {
+            ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+        }
+
+        private async void Download(object sender, EventArgs e)
+        {
+            string url = urlEntry.Text;
+
+            if (url.Contains("github.com"))
+            {
+                string[] urlSplit = url.Split('/');
+                string repoName = urlSplit[urlSplit.Length - 1];
+                string repoOwner = urlSplit[urlSplit.Length - 2];
+                string repoURL = "http://github.com/" + repoOwner + "/" + repoName + "/archive/refs/heads/main.zip";
+                string savePath = savePathEntry.Text;
+
+                if (savePath == "")
+                {
+                    savePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                }
+
+                string saveFile = savePath + "\\" + repoName + ".zip";
+
+                // Download the file from the URL
+                using (HttpClient client = new HttpClient())
+                {
+
+                    // Save the file to the specified path
+                    try
+                    {
+                        BypassCertificateValidation();
+                        HttpResponseMessage response = await client.GetAsync(repoURL);
+                        byte[] content = await response.Content.ReadAsByteArrayAsync();
+
+                        File.WriteAllBytes(saveFile, content);
+
+                        // Extract the file
+                        ZipFile.ExtractToDirectory(saveFile, savePath);
+
+                        // Delete the zip file
+                        File.Delete(saveFile);
+
+                        MessageBox.Show("Download complete!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Download failed!\n\n" + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid GitHub URL");
+            }
+        }
+
+        private void PopulateDownloadPath(object sender, EventArgs e)
+        {
+            // Ask the user to select a folder to save the file to
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            folderBrowserDialog.Description = "Select a folder to save the file to...";
+            folderBrowserDialog.ShowNewFolderButton = true;
+
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                savePathEntry.Text = folderBrowserDialog.SelectedPath;
+            }
+        }
     }
 }
