5fe3fb6573d53e0f55a201202ca3ebc52a92c57e
Alexander Sanderson <67873187+Vexelior@users.noreply.github.com>
2023-02-09 16:45:20 -0800

    add icon, labels for entries

--- App/Repo Downloader/Form1.Designer.cs
+++ App/Repo Downloader/Form1.Designer.cs
@@ -29,17 +29,20 @@ namespace Repo_Downloader
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.submitButton = new System.Windows.Forms.Button();
             this.urlEntry = new System.Windows.Forms.TextBox();
             this.outputBox = new System.Windows.Forms.TextBox();
             this.title = new System.Windows.Forms.Label();
             this.savePathEntry = new System.Windows.Forms.TextBox();
             this.pathButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // submitButton
             // 
-            this.submitButton.Location = new System.Drawing.Point(325, 139);
+            this.submitButton.Location = new System.Drawing.Point(322, 178);
             this.submitButton.Name = "submitButton";
             this.submitButton.Size = new System.Drawing.Size(156, 43);
             this.submitButton.TabIndex = 0;
@@ -49,7 +52,7 @@ namespace Repo_Downloader
             // 
             // urlEntry
             // 
-            this.urlEntry.Location = new System.Drawing.Point(63, 73);
+            this.urlEntry.Location = new System.Drawing.Point(63, 96);
             this.urlEntry.Name = "urlEntry";
             this.urlEntry.Size = new System.Drawing.Size(693, 23);
             this.urlEntry.TabIndex = 1;
@@ -58,12 +61,12 @@ namespace Repo_Downloader
             // 
             this.outputBox.AcceptsReturn = true;
             this.outputBox.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.outputBox.Location = new System.Drawing.Point(12, 188);
+            this.outputBox.Location = new System.Drawing.Point(12, 227);
             this.outputBox.Multiline = true;
             this.outputBox.Name = "outputBox";
             this.outputBox.ReadOnly = true;
             this.outputBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.outputBox.Size = new System.Drawing.Size(776, 250);
+            this.outputBox.Size = new System.Drawing.Size(776, 211);
             this.outputBox.TabIndex = 3;
             // 
             // title
@@ -78,14 +81,14 @@ namespace Repo_Downloader
             // 
             // savePathEntry
             // 
-            this.savePathEntry.Location = new System.Drawing.Point(63, 110);
+            this.savePathEntry.Location = new System.Drawing.Point(63, 149);
             this.savePathEntry.Name = "savePathEntry";
             this.savePathEntry.Size = new System.Drawing.Size(693, 23);
             this.savePathEntry.TabIndex = 5;
             // 
             // pathButton
             // 
-            this.pathButton.Location = new System.Drawing.Point(763, 110);
+            this.pathButton.Location = new System.Drawing.Point(762, 149);
             this.pathButton.Name = "pathButton";
             this.pathButton.Size = new System.Drawing.Size(25, 23);
             this.pathButton.TabIndex = 6;
@@ -93,19 +96,40 @@ namespace Repo_Downloader
             this.pathButton.UseVisualStyleBackColor = true;
             this.pathButton.Click += new System.EventHandler(this.PopulateDownloadPath);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(363, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 15);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "GitHub Link";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(351, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Output Directory";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.pathButton);
             this.Controls.Add(this.savePathEntry);
             this.Controls.Add(this.title);
             this.Controls.Add(this.outputBox);
             this.Controls.Add(this.urlEntry);
             this.Controls.Add(this.submitButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Repository Downloader";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -119,6 +143,8 @@ namespace Repo_Downloader
         private System.Windows.Forms.Label title;
         private System.Windows.Forms.TextBox savePathEntry;
         private System.Windows.Forms.Button pathButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
